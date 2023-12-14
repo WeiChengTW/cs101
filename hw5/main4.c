6fcadaad6d43ac816b86ad617da4f1f3470fe0e9
Wei_Cheng Hung <138794467+WeiChengTW@users.noreply.github.com>
2023-12-14 15:52:42 +0800

    Create main4.c

---
+++ hw5/main4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int N =12345;
+    int x1;
+    int x2;
+    int x3;
+    int x4;
+    int x5;
+
+    for(int i;i<=5;i++){
+        x=N/10;
+    }
+    return 0;
+}
