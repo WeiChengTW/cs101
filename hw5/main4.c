2f4d1b0eebbc3d50b2ee602e0973ffc5c6ee2194
Wei_Cheng Hung <138794467+WeiChengTW@users.noreply.github.com>
2023-12-14 22:36:08 +0800

    Update main4.c

--- hw5/main4.c
+++ hw5/main4.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
-int main(){
-    int N =12345;
-    int x1;
-    int x2;
-    int x3;
-    int x4;
-    int x5;
+int main()
+{
+    int num = 1; // 126453
+    // printf("num=%d\n", num);
+    int more_thousand = num - (num % 10000);
+    int thousands = num / 1000 % 10;
+    int hundreds = num / 100 % 10;
+    int tens = num / 10 % 10;
+    int ones = num / 1 % 10;
+    // printf("ones=%d\n", ones);
+    // printf("tens=%d\n", tens);
+    // printf("hundreds=%d\n", hundreds);
+    // printf("thousands=%d\n", thousands);
 
-    for(int i;i<=5;i++){
-        x=N/10;
-    }
+    // Swap thousands and ones
+    int temp = thousands;
+    thousands = ones;
+    ones = temp;
+    // printf("new_ones=%d\n", ones);
+    // printf("new_thousands=%d\n", thousands);
+
+    int ans_num = more_thousand + ones * 1 + tens * 10 + hundreds * 100 + thousands * 1000;
+    printf("ans_num=%d\n", ans_num);
     return 0;
 }
